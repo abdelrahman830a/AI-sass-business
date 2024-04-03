965f85bcf7a0d0b6c9e58bdb09a76d05e4748fb6
abdelrahman830a <abdelrahman.1220005@stemalex.moe.edu.eg>
2024-04-03 02:58:29 +0200

    Implemented Sidebar and MobileNav

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,83 @@
+"use client";
+import React from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { navLinks } from "@/contstants";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
+
+const MobileNav = () => {
+  const pathname = usePathname();
+  return (
+    <header className="header">
+      <Link href="/" className="flex items-center gap-2 md:py-2">
+        <Image
+          src="/assets/images/logo-text.svg"
+          alt="logo"
+          width={180}
+          height={28}
+        />
+      </Link>
+      <nav className="flex gap-2">
+        <SignedIn>
+          <UserButton afterSignOutUrl="/" />
+          <Sheet>
+            <SheetTrigger>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
+            </SheetTrigger>
+            <SheetContent className="sheet-content sm:w-64">
+              <>
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  alt="logo"
+                  width={152}
+                  height={23}
+                />
+                <ul className="header-nav_elements">
+                  {navLinks.map((link) => {
+                    const isActive = pathname === link.route;
+                    return (
+                      <li
+                        key={link.route}
+                        className={`whitespace-nowrap p-18 flex text-dark-700 cursor-pointer ${
+                          isActive && "gradient-text"
+                        }`}>
+                        <Link href={link.route} className="sidebar-link">
+                          <Image
+                            src={link.icon}
+                            alt="icon"
+                            width={24}
+                            height={24}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                  <li className="flex-center cursor-pointer gap-2 p-4">
+                    <UserButton afterSignOutUrl="/" showName />
+                  </li>
+                </ul>
+              </>
+            </SheetContent>
+          </Sheet>
+        </SignedIn>
+        <SignedOut>
+          <Button className="button bg-purple-gradient bg-cover button">
+            <Link href="/sign-in">Login</Link>
+          </Button>
+        </SignedOut>
+      </nav>
+    </header>
+  );
+};
+
+export default MobileNav;
