ccf8584e2f928bf1e6a316ec5df935c2a5bcda2d
abdelrahman830a <abdelrahman.1220005@stemalex.moe.edu.eg>
2024-04-05 11:24:26 +0200

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({});
+export default authMiddleware({
+    publicRoutes: [
+        "/api/webhooks(.*)"
+    ],
+
+});
 
 export const config = {
     matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
