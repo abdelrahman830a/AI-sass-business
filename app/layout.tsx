002114cf7585586071eebf42c4ff1f77bfff038a
abdelrahman830a <abdelrahman.1220005@stemalex.moe.edu.eg>
2024-04-07 21:47:40 +0200

    Rebranding and name change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Imagagine - AI-powered",
+  title: "Imagaginify - Images for the future",
   description: "AI-powered image generator",
 };
 
